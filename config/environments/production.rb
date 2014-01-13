41ee9994faa35f9efeaa4937633065da597ee3ca
Nolan Morache <nolan241@gmail.com>
2014-01-13 13:36:23 -0800

    Implemented new users devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,9 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+#set this to your actual host name for devise setup
+config.action_mailer.default_url_options = { :host => 'http://cryptic-dawn-6388.herokuapp.com/' }
+
 end
